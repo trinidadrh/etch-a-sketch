2f3f62553eb690be775f8756c2c6efa4fbee0107
Trinidad <trinidadrh@gmail.com>
2021-11-27 12:31:28 -0600

    Share for hover feedback

--- file.js
+++ file.js
@@ -1,8 +1,19 @@
-const container = document.getElementById("container");
-
+// Prompts user to choose number of rows/columns
 let chooseNumOfRows = prompt("Select the number of rows/columns on your sketchpad.")
 
+// Creates 16x16 grid
+let container = document.getElementById("container");
+
 for (let i = 0; i < 256; i++) {
-    container.innerHTML += '<div class="box"></div>';
+    container.innerHTML += '<div class="box-item"></div>';
 }
+
+// Trigger function when mouse enters box
+let boxChange = document.querySelector(".box-item");
+
+
+boxChange.addEventListener("mouseenter", hoverChange);
+function hoverChange() {
+    boxChange.style.backgroundColor = "black";
+};
 
